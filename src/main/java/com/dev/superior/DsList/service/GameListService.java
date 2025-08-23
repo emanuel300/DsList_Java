e5e503eac46c0565abf48ce8914026f22fc19fb7
Emanuel <fergimanu3@gmail.com>
2025-08-23 16:18:08 -0300

    chore: subir código do endpoint /replacement para analisar problema de injeção

--- src/main/java/com/dev/superior/DsList/service/GameListService.java
+++ src/main/java/com/dev/superior/DsList/service/GameListService.java
@@ -4,8 +4,10 @@ import com.dev.superior.DsList.dto.GameDto;
 import com.dev.superior.DsList.dto.GameListDto;
 import com.dev.superior.DsList.entities.Game;
 import com.dev.superior.DsList.entities.GameList;
+import com.dev.superior.DsList.projections.GameMinProjection;
 import com.dev.superior.DsList.repository.GameListRepository;
 import com.dev.superior.DsList.repository.GameRepository;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,9 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
 
     @Transactional(readOnly = true)
     public List<GameListDto> gameFindAll(){
@@ -25,6 +30,21 @@ public class GameListService {
                 .map(x -> new GameListDto(x))
                 .toList();
 
+    }
+
+    @Transactional
+    public void move(long listId, int sourceIndex, int destinationIndex){
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj =  list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = destinationIndex < sourceIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i < max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 
 
